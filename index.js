39ac63e128e88ac3033f43cea9d900367d814735
Elizabeth Clawson <emclawson1@gmail.com>
2021-06-20 20:38:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const name = ['Lisa','Kaitlin','Jan'];
+const event = ['birthday'];
+
+function writeCards(name, event) {
+  let newArr = [];
+  for (let i = 0; i < name.length; i++) {
+    newArr.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    debugger;
+  }
+  return newArr;
+}
+
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  } 
+}
